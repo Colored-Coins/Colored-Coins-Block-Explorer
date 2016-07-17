09f26de9e3f4037141463dcc6c4a70a68be0530d
Oded Leiba <oded@colu.com>
2016-07-17 13:11:32 +0300

    retry parse_tx on transaction rollback

--- app/controllers/api.js
+++ app/controllers/api.js
@@ -272,8 +272,16 @@ var get_utxos = function (req, res, next) {
 var parse_tx = function (req, res, next) {
   var params = req.data
   var txid = params.txid || ''
+  function isTransactionRollbackError (err) {
+    var errorCode = err && (err.code || (err.original && err.original.code))
+    return errorCode && errorCode.length === 5 && errorCode.substring(0, 2) === '40' // PostgreSQL Transaction Rollback prefix
+  }
   console.time('parse_tx: full_parse ' + txid)
   scanner.priority_parse(txid, function (err) {
+    if (isTransactionRollbackError(err)) {
+      console.log('parse_tx failed with transaction rollback error, retrying ' + txid)
+      return parse_tx(req, res, next)
+    }
     if (err) return next(err)
     console.timeEnd('parse_tx: full_parse ' + txid)
     res.send({txid: txid})
@@ -642,7 +650,7 @@ var find_addresses_info = function (addresses, confirmations, callback) {
     .then(function (addresses_info) {
       var results = {}
       addresses_info.forEach(function (address_info) {
-        var utxos = address_info.utxos = []
+        var utxos = []
         var assets = {}
         address_info.balance = 0
         address_info.received = 0
@@ -873,7 +881,10 @@ var find_asset_holders = function (assetId, confirmations, callback) {
   sequelize.query(find_asset_holders_query, {type: sequelize.QueryTypes.SELECT, replacements: {assetId: assetId}, logging: console.log, benchmark: true})
     .then(function (holders) {
       if (!holders.length) {
-        return callback(null, [])
+        return callback(null, {
+          assetId: assetId,
+          holders: []
+        })
       }
       var ans = {}
       ans.assetId = assetId
